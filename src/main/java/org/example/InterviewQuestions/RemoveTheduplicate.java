c05032fabf5b0c3acbd777754f72eec9c0a4976d
surajsap <suraj-sambhaji.patil@capgemini.com>
2025-07-03 15:12:59 +0530

    changes code

---
+++ src/main/java/org/example/InterviewQuestions/RemoveTheduplicate.java
@@ -0,0 +1,18 @@
+package org.example.InterviewQuestions;
+
+public class RemoveTheduplicate {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Array before removing duplicates: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Logic to remove duplicates can be added here
+    }
+}
